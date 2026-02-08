925d5baf96ef0a1922de607cd2b428201fe6e45c
DrCantagalo <drmichelcantagalo@gmail.com>
2026-02-08 14:16:24 +0100

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'passport', // Aqui é onde a mágica acontece
+            'provider' => 'users',
+        ],
     ],
 
     /*
